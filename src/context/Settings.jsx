109acee0912aa24df9bb717734964a976ae16560
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-30 14:01:52 -0700

    added Settings context component

---
+++ src/context/Settings.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+// This is the object that will track values
+export const SettingsContext = React.createContext();
+
+// this is the Provider that must be present at the root of our app.
+function SettingsProvider(props) {
+  const [display, setDisplay] = useState(3);
+  const [completed, setSetCompleted] = useState(true);
+
+  return (
+    // the value prop must be called value
+    <SettingsContext.Provider value={{ display, completed }}>
+      {/* this is not banana */}
+      {props.children}
+    </SettingsContext.Provider>
+  );
+}
+
+export default SettingsProvider;
