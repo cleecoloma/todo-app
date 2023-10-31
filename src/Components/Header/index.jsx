6c0ec75dd1ff65046c30023c9535b09a5a58f7cb
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-31 15:52:24 -0700

    updated header styling

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -6,23 +6,18 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <Navbar className='custom-navbar' expand='lg'>
-      <div
-        className='mx-auto d-flex justify-content-between align-items-center'
-        style={{ width: '40%' }}
-      >
-        <Nav>
-          <Nav.Item>
-            <Link className='nav-link custom-nav-link' to='/'>
-              Home
-            </Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link className='nav-link custom-nav-link' to='/Settings'>
-              Settings
-            </Link>
-          </Nav.Item>
-        </Nav>
-      </div>
+      <Nav className='nav'>
+        <Nav.Item className='nav-item'>
+          <Link className='nav-link custom-nav-link' to='/'>
+            Home
+          </Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Link className='nav-link custom-nav-link' to='/Settings'>
+            Settings
+          </Link>
+        </Nav.Item>
+      </Nav>
     </Navbar>
   );
 }
