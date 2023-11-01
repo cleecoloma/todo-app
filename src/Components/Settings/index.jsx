373d32ae2a8ba70408ebf0417e5575f187e717ae
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-31 19:54:38 -0700

    updated todo styling and ability to change display numbers

--- src/Components/Settings/index.jsx
+++ src/Components/Settings/index.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { SettingsContext } from '../../Context/Settings';
+import Footer from '../Footer';
+import { Switch, NumberInput } from '@mantine/core';
 
 function Settings() {
+  const { display, isCompleted, updateDisplay, toggleIsCompleted } =
+    useContext(SettingsContext);
+
+  const handleDisplayChange = (value) => {
+    updateDisplay(value);
+  };
+
+  const handleToggleIsCompleted = () => {
+    toggleIsCompleted();
+  };
+
   return (
-    <div>Settings</div>
-  )
+    <>
+      <div>
+        <p>Display: {display}</p>
+        <p>Is Completed: {isCompleted ? 'Yes' : 'No'}</p>
+        <button onClick={handleDisplayChange}>Change Display</button>
+        <button onClick={handleToggleIsCompleted}>Toggle Is Completed</button>
+        <Switch checked={isCompleted} onChange={handleToggleIsCompleted} />
+        <NumberInput
+          defaultValue={display}
+          min={1}
+          max={5}
+          onChange={handleDisplayChange}
+        />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default Settings
+export default Settings;
--- src/Context/Settings/index.jsx
+++ src/Context/Settings/index.jsx
@@ -6,8 +6,21 @@ function SettingsProvider(props) {
   const [display, setDisplay] = useState(3);
   const [isCompleted, setIsCompleted] = useState(false);
 
+    const updateDisplay = (newDisplay) => {
+      if (newDisplay >= 1 && newDisplay <= 5) {
+        setDisplay(newDisplay);
+      }
+    };
+
+    const toggleIsCompleted = () => {
+      setIsCompleted(!isCompleted);
+    };
+
+
   return (
-    <SettingsContext.Provider value={{ display, isCompleted }}>
+    <SettingsContext.Provider
+      value={{ display, isCompleted, updateDisplay, toggleIsCompleted }}
+    >
       {props.children}
     </SettingsContext.Provider>
   );
