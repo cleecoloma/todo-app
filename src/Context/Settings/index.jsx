57769573321bcabd1f6e354e91cfd3ee7da67b71
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-31 21:51:38 -0700

    updated settings context provider with save to local functionality

--- src/Context/Settings/index.jsx
+++ src/Context/Settings/index.jsx
@@ -1,21 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const SettingsContext = React.createContext();
 
 function SettingsProvider(props) {
-  const [display, setDisplay] = useState(3);
-  const [isCompleted, setIsCompleted] = useState(false);
 
-    const updateDisplay = (newDisplay) => {
-      if (newDisplay >= 1 && newDisplay <= 5) {
-        setDisplay(newDisplay);
-      }
-    };
+  const initialUserPreferences = JSON.parse(
+    localStorage.getItem('userPreferences')
+  );
+  const [display, setDisplay] = useState(
+    initialUserPreferences ? initialUserPreferences.display : 3
+  );
+  const [isCompleted, setIsCompleted] = useState(
+    initialUserPreferences ? initialUserPreferences.isCompleted : false
+  );
 
-    const toggleIsCompleted = () => {
-      setIsCompleted(!isCompleted);
-    };
+  const updateDisplay = (newDisplay) => {
+    if (newDisplay >= 1 && newDisplay <= 5) {
+      setDisplay(newDisplay);
+    }
+  };
 
+  const toggleIsCompleted = () => {
+    setIsCompleted(!isCompleted);
+  };
+
+  // Save user preferences to local storage
+  const saveUserPreferencesToLocalStorage = () => {
+    const userPreferences = { display, isCompleted };
+    localStorage.setItem('userPreferences', JSON.stringify(userPreferences));
+  };
+
+  // Read from local storage and set the state properties on load
+  useEffect(() => {
+    const userPreferences = localStorage.getItem('userPreferences');
+    if (userPreferences) {
+      const parsedPreferences = JSON.parse(userPreferences);
+      setDisplay(parsedPreferences.display);
+      setIsCompleted(parsedPreferences.isCompleted);
+    }
+  }, []);
+
+  useEffect(() => {
+    saveUserPreferencesToLocalStorage();
+  }, [display, isCompleted]);
 
   return (
     <SettingsContext.Provider
